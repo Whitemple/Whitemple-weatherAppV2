ff7fb4f39dd3ece4f75807bd9ee38ddccec6820a
whitemple <yarikkhromey@mail.ru>
2023-03-02 20:38:40 +0300

    create fetch function

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,27 @@
-console.log("hello");
+// const app = document.querySelector("#app");
+// const createPage = () => {
+//   console.log("NICE");
+// };
+// createPage();
+
+const form = document.querySelector("#form");
+const input = document.querySelector("#inputCity");
+
+let city;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  city = input.value.trim();
+  console.log(city);
+  input.value = "";
+  const apiKey = "1289f3b71732cf788b8ea917a6299964";
+  const url = `https://api.openweathermap.org/data/2.5/weather?
+  q=${city}&appid=${apiKey}&lang=ru`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      console.log(data.name);
+      console.log(data.main.temp);
+    });
+});
